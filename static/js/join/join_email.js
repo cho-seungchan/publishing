55a5cc51fb72fe039f166839c0bb163d77198e21
dev.parkjk9425 <dev.parkjk9425@gmail.com>
2025-02-03 08:56:32 +0900

    250203 0856

--- static/js/join/join_email.js
+++ static/js/join/join_email.js
@@ -1,18 +1,22 @@
-const lastP = document.querySelector(".fLECcs>div");
+const div = document.querySelector(".fLECcs>div");
 const inputEmail = document.querySelector(".inputEmail");
 const p = document.createElement("p");
+const lastP = div.lastElementChild;
+
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const passwordRegex =
     /^(?=.*[A-Za-z].*)(?=.*\d.*|.*[\W_].*)[A-Za-z\d\W_]{10,}$/;
 
-p.innerHTML = `<p class = "bViOzS">올바른 이메일 형식이 아닙니다.</p>`;
+// p.innerHTML = `<p class = "bViOzS">올바른 이메일 형식이 아닙니다.</p>`;
 
 inputEmail.addEventListener("blur", () => {
     if (!emailRegex.test(inputEmail.value)) {
         inputEmail.style.border = "1px solid rgb(222, 28, 34)";
-        inputEmail.after(p);
+        inputEmail.after(
+            p
+        ).innerHTML = `<p class = "bViOzS">올바른 이메일 형식이 아닙니다.</p>`;
         return;
     }
     inputEmail.style.border = "1px solid rgb(238, 238, 238)";
-    inputEmail.remove;
+    lastP.remove();
 });
