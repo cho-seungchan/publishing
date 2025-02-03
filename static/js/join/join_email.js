7c4500406a3dc9d56d1c165bb4e14f687d2ea06b
dev.parkjk9425 <dev.parkjk9425@gmail.com>
2025-02-03 09:25:58 +0900

    250203 0925

--- static/js/join/join_email.js
+++ static/js/join/join_email.js
@@ -7,14 +7,12 @@ const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const passwordRegex =
     /^(?=.*[A-Za-z].*)(?=.*\d.*|.*[\W_].*)[A-Za-z\d\W_]{10,}$/;
 
-// p.innerHTML = `<p class = "bViOzS">올바른 이메일 형식이 아닙니다.</p>`;
+p.innerHTML = `<p class = "bViOzS">올바른 이메일 형식이 아닙니다.</p>`;
 
 inputEmail.addEventListener("blur", () => {
     if (!emailRegex.test(inputEmail.value)) {
         inputEmail.style.border = "1px solid rgb(222, 28, 34)";
-        inputEmail.after(
-            p
-        ).innerHTML = `<p class = "bViOzS">올바른 이메일 형식이 아닙니다.</p>`;
+        inputEmail.after(p);
         return;
     }
     inputEmail.style.border = "1px solid rgb(238, 238, 238)";
